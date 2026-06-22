9eb61b1d385cd219d91e733127364474906fdbac
Debanjan Das <microsoft2420@gmail.com>
2026-06-22 12:38:27 +0530

    Update svgs for proper shape and sizing

--- src/assets/dark-mode-svg.jsx
+++ src/assets/dark-mode-svg.jsx
@@ -1,6 +1,13 @@
 ﻿export function darkMode() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="#1a1a1a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <svg xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#000"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="w-6 h-6">
             <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
         </svg>
     )
